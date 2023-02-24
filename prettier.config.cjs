99e670baf8f251600364d8378de6eab3fd254d9c
zhaosigui <zhaosigui@126.com>
2023-02-24 17:22:27 +0800

    ✨ feat(增加prettier):

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 换行字符串阈值
+  semi: true, //句尾必须加分号
+  vueIndentScriptAndStyle: true,
+  singleQuote: true,  //单引号
+  trailingComma: 'all', // 最后一个对象元素加逗号
+  proseWrap: 'never', //代码超出是否要换行 preserve保留,never不保留
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+};
