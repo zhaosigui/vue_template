736d97ba829e1515690b5dd535b5d90a18a9adc2
zhaosigui <zhaosigui@126.com>
2023-02-27 13:38:34 +0800

    chore(commit-msg): 添加git提交规范

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -105,4 +105,131 @@
 //     // customScopesAlias: 'custom:     自定义',
 //   },
 // };
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+        'release',
+      ],
+    ],
+  },
+  prompt: {
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐞',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📃',
+          },
+          style: {
+            description:
+              'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '🌈',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '🦄',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🎈',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🧪',
+          },
+          build: {
+            description:
+              'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '🔧',
+          },
+          ci: {
+            description:
+              'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '🐎',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '🐳',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '↩',
+          },
+        },
+      },
+      // scope: {
+      //   description: 'What is the scope of this change (e.g. component or file name)',
+      // },
+      // subject: {
+      //   description: 'Write a short, imperative tense description of the change',
+      // },
+      // body: {
+      //   description: 'Provide a longer description of the change',
+      // },
+      // isBreaking: {
+      //   description: 'Are there any breaking changes?',
+      // },
+      // breakingBody: {
+      //   description:
+      //     'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+      // },
+      // breaking: {
+      //   description: 'Describe the breaking changes',
+      // },
+      // isIssueAffected: {
+      //   description: 'Does this change affect any open issues?',
+      // },
+      // issuesBody: {
+      //   description:
+      //     'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+      // },
+      // issues: {
+      //   description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      // },
+    },
+  },
+};
